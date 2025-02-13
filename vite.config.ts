03b329f16f16ee9bc44455596b03bad19ab5b05b
tony <tony.payet.professionnel@gmail.com>
2025-02-13 19:17:47 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // Permet d'écouter sur toutes les interfaces réseau
-    port: 5173,       // Assurez-vous que le port 5173 est bien exposé
+    port: 3000,       // Assurez-vous que le port 5173 est bien exposé
   },
   optimizeDeps: {
     include: ['react', 'react-dom'],
