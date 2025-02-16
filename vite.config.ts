09528403cc4d236f14ad6a4e557800fc0095dca6
tony <tony.payet.professionnel@gmail.com>
2025-02-16 11:49:31 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0',  // Permet à Vite d'écouter sur toutes les interfaces réseau
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'http://host.docker.internal:5001',  // Change localhost par host.docker.internal
         changeOrigin: true,
-        secure: false
+        secure: false,
       }
     }
   },
