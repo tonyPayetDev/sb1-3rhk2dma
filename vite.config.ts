f70aff20f54cfd68c74df04d886bbcce7eff748f
tony <tony.payet.professionnel@gmail.com>
2025-02-16 23:57:19 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
    proxy: {
   '/api': {
-    target: 'http://localhost:5001',  // ou une autre IP de ton hôte
+    target: 'http://localhost:5002',  // ou une autre IP de ton hôte
     changeOrigin: true,
     secure: false
   }
