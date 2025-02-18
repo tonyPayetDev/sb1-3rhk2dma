b9d22cb4e09afc6ec499f1851ffe35194a8e2047
tony <tony.payet.professionnel@gmail.com>
2025-02-18 20:50:31 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Permet d'écouter sur toutes les interfaces réseau
-    port: 5000,       // Assurez-vous que le port 5173 est bien exposé
+    host: '0.0.0.0', // Permet d'écouter sur toutes les interfaces réseau
+    port: 5000, // Le port que vous souhaitez utiliser
+    https: false, // Désactive HTTPS
   },
   optimizeDeps: {
-    include: ['react', 'react-dom'],
-    exclude: ['@remotion/player', 'remotion'],
-  }
+    include: ["react", "react-dom"],
+    exclude: ["@remotion/player", "remotion"],
+  },
 });
