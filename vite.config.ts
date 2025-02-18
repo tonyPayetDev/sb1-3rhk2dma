cb21d8bf399e2db6dc82dadeae56b0b94919a499
tony <tony.payet.professionnel@gmail.com>
2025-02-18 10:04:08 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-}
-  },
   optimizeDeps: {
     include: ['react', 'react-dom'],
     exclude: ['@remotion/player', 'remotion'],
