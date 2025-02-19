55b3ad7ba3d7d8d377dc51fbcabf914c7c29a543
tony <tony.payet.professionnel@gmail.com>
2025-02-19 10:53:35 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Permet d'écouter sur toutes les interfaces réseau
-    port: 5000, // Le port que vous souhaitez utiliser
-    https: false, // Désactive HTTPS
+    host: '0.0.0.0', // Écoute sur toutes les interfaces
+    port: 5000, // Port utilisé en local
+    https: false, // Coolify gère HTTPS
+    proxy: {
+      "/api": {
+        target: "https://dev.tonypayet.com", // Redirige /api vers le backend HTTPS
+        changeOrigin: true,
+        secure: true,
+      }
+    }
   },
   optimizeDeps: {
     include: ["react", "react-dom"],
