36bf9b4c2c8f1ae418798ef256d7177cb1a085b0
tony <tony.payet.professionnel@gmail.com>
2025-02-19 11:39:55 +0400

    Update VideoGenerator.tsx

--- src/components/VideoGenerator.tsx
+++ src/components/VideoGenerator.tsx
@@ -189,7 +189,7 @@ export function VideoGenerator({
     try {
       setIsRendering(true);
       // http://l88ggc8w4w00o0oww48k4gk4.45.90.121.197.sslip.io/ https://m6hl5l-5000.csb.app
-      const response = await fetch("http://dev.tonypayet.com:5000/api/render", {
+      const response = await fetch("https://dev.tonypayet.com/api/render", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
